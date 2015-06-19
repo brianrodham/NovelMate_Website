d1d377e8022a2a03ce72e4ce2eaecea50704b7b9
Brian Rodham <brian.j.rodham@gmail.com>
2015-06-19 00:11:18 -0600

    Added mockup for requesting access
    - Added text that pops up when you submit your email
    - Added email validation
    - Changed bottom request form to just be a button that redirects to the
    top.
    - Added a note reminding me that I want to put images with shadowboxes
    at the bottom
    - Previously this was going to be a video

---
+++ NovelMate_Website/Scripts/request-access-submit.js
@@ -0,0 +1,29 @@
+﻿function submitEarlyAccess() {
+        
+    var email = $(".input").val();
+    $(".success-text").show();
+
+    return false;
+}
+
+function ValidateEmail(email)
+{
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+}
+
+
+$('.earlyAccessForm').submit(function (event) {
+    event.preventDefault();
+    var email = $(".input").val();
+    if (ValidateEmail(email)) {
+        $(".fail-text").hide();
+        $(".success-text").show();
+    }
+    else {
+        $(".fail-text").show();
+        $(".success-text").hide();
+    }
+   
+
+});
